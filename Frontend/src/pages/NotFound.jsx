52bce0f979d42dde8184be35f51a1c4a2eb4ce1f
marialucialombardi90 <lombardi074@gmail.com>
2024-09-10 18:13:47 +0500

    deleted src

--- Frontend/src/pages/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-function NotFound() {
-    return (
-        <h1 className="text-center">Ooops!Page not found</h1>
-    )
-}
-
-export default NotFound;
--- Frontend/src/utils.js
+++
@@ -1,3 +0,0 @@
-export const serverConfig = {
-    base_url: process.env.REACT_APP_BASE_URL
-}
